5201286022aa5b348c823d25a9f5bd6ba5dc1fa5
iadcruz <iadcruzsa21@gmail.com>
2025-08-07 20:20:43 -0400

    Switch logo

--- src/lib/components/ExperienceCard/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard/ExperienceCard.svelte
@@ -40,6 +40,7 @@
 	color={experience.color}
 >
 	<div class="col md:flex-row items-start gap-5 md:gap-1">
+		<CardLogo src={getAssetURL(experience.logo)} alt={experience.company} size={55} />
 		<div class="col ml-0 md:ml-[20px] gap-3 w-full">
 			<div class="col ">
 				<h3
@@ -49,7 +50,6 @@
 				</h3>
 			</div>
 		</div>
-		<CardLogo src={getAssetURL(experience.logo)} alt={experience.company} size={55} />
 	</div>
 	<div class="mt-3 row flex-wrap items-start m-b-2 gap-1 text-0.9em font-300">
 		{#each info as item}
